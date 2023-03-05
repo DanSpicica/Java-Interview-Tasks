2a002702f783475835e6a42fc65b339f80af4486
SpicicaDan <spicica.jr@gmail.com>
2023-03-05 14:58:40 -0800

    frequency of character

---
+++ src/FrequencyOfCharacter.java
@@ -0,0 +1,31 @@
+public class FrequencyOfCharacter {
+
+    public static void main(String[] args) {
+        frequencyOfCharacterInString("asdfasdfasfdgsdfhfjfdgj");
+    }
+
+    public static void frequencyOfCharacterInString(String str){
+
+        String result="";
+
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+            int count=0;
+            for (int j = 0; j < str.length(); j++) {
+                if (str.charAt(j)==ch){
+                    count++;
+                }
+
+            }
+            if (result.contains(""+ch)){
+                continue;
+            }
+            result+=ch+""+count;
+
+        }
+        System.out.println(result);
+
+    }
+
+}
